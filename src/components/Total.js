32eca92a7b2f0feca87359d5f97e046a7b857f5b
emilysamantha <emilyzarry@gmail.com>
2023-02-02 12:40:34 +1100

    Implement month picker

--- src/components/Total.js
+++ src/components/Total.js
@@ -2,12 +2,34 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import { numberWithCommas } from "../utils/format";
 
-const Total = ({ title }) => {
+const Total = ({ title, calculate, month, year }) => {
   const { transactions } = useContext(GlobalContext);
-  const amounts = transactions.map((transaction) => transaction.amount);
-  const total = amounts
-    .reduce((acc, amount) => (acc += parseFloat(amount)), 0)
-    .toFixed(2);
+  let amounts = 0;
+  let total = 0;
+
+  if (month < 0) {
+    amounts = transactions.map((transaction) => transaction.amount);
+  } else {
+    amounts = transactions
+      .filter(
+        (transaction) =>
+          transaction.date.getMonth() == month &&
+          transaction.date.getFullYear() == year
+      )
+      .map((transaction) => transaction.amount);
+  }
+
+  switch (calculate) {
+    case "balance":
+      total = amounts
+        .reduce((acc, amount) => (acc += parseFloat(amount)), 0)
+        .toFixed(2);
+    case "income":
+      total = amounts
+        .filter((amount) => amount > 0)
+        .reduce((acc, amount) => (acc += parseFloat(amount)), 0)
+        .toFixed(2);
+  }
 
   return (
     <div>
--- src/pages/IncomeSummary.js
+++ src/pages/IncomeSummary.js
@@ -1,10 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Header from "../components/Header";
+import Total from "../components/Total";
 import ToggleButton from "../components/ToggleButton";
 import { GlobalContext } from "../context/GlobalState";
 
 const IncomeSummary = () => {
+  // Get current date
+  let currentDate = new Date();
+
   const { theme } = useContext(GlobalContext);
+  const [chosenDate, setChosenDate] = useState(currentDate);
+
+  // Function to decrease month
+  function decMonth() {
+    let dateDec = new Date(chosenDate);
+    dateDec.setMonth(dateDec.getMonth() - 1);
+    setChosenDate(dateDec);
+  }
+
+  // Function to increase month
+  function incMonth() {
+    let dateInc = new Date(chosenDate);
+    dateInc.setMonth(dateInc.getMonth() + 1);
+    setChosenDate(dateInc);
+  }
+
   return (
     <div
       className={
@@ -14,6 +34,19 @@ const IncomeSummary = () => {
       <div className="header">
         <Header title="Income Summary" />
         <ToggleButton />
+      </div>
+      <div className="month-picker">
+        <i class="fa-solid fa-chevron-left" onClick={() => decMonth()}></i>
+        <h4>{chosenDate.toLocaleString("default", { month: "long" })}</h4>
+        <i class="fa-solid fa-chevron-right" onClick={() => incMonth()}></i>
+      </div>
+      <div className="exp-tracker-container">
+        <Total
+          title="Total Income"
+          calculate="income"
+          month={chosenDate.getMonth()}
+          year={chosenDate.getFullYear()}
+        />
       </div>
     </div>
   );
