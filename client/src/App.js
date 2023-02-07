591155fc3028295323427377d8525e94077fdd16
emilysamantha <emilyzarry@gmail.com>
2023-02-07 16:58:32 +1100

    Fix routiong for github pages

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/incomesummary" element={<Summary incExp="inc" />} />
-        <Route path="/expensesummary" element={<Summary incExp="exp" />} />
+        <Route path="my-expense-tracker/" element={<Home />} />
+        <Route
+          path="my-expense-tracker/incomesummary"
+          element={<Summary incExp="inc" />}
+        />
+        <Route
+          path="my-expense-tracker/expensesummary"
+          element={<Summary incExp="exp" />}
+        />
       </Routes>
     </BrowserRouter>
   );
